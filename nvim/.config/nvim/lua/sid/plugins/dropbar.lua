4b24fc759545998ee96bebe6b6dac5343129edfd
Sidharthan Kamaraj <csidharthank@gmail.com>
2024-10-28 10:10:16 +0100

    feat: add dropbar

---
+++ nvim/.config/nvim/lua/sid/plugins/dropbar.lua
@@ -0,0 +1,8 @@
+-- return {
+-- 	'Bekaboo/dropbar.nvim',
+-- 	dependencies = {
+-- 		'nvim-telescope/telescope-fzf-native.nvim',
+-- 	},
+-- }
+-- disabling this plugin for now since I'm not using it.
+return {}
