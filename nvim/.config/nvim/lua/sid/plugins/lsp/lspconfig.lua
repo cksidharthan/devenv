576aa4932fbb1e44f8d3032991084b52d4691988
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-11-24 23:44:26 +0530

    feat: add neoscroll and smearcursor for having cursor trial like in neovide

--- nvim/.config/nvim/lua/sid/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/sid/plugins/lsp/lspconfig.lua
@@ -1,27 +1,26 @@
 return {
-	"neovim/nvim-lspconfig",
-  event = "VeryLazy",
+	'neovim/nvim-lspconfig',
+	event = 'VeryLazy',
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"folke/lazydev.nvim",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
-		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-		"jose-elias-alvarez/typescript.nvim",
+		'hrsh7th/cmp-nvim-lsp',
+		'folke/lazydev.nvim',
+		{ 'antosha417/nvim-lsp-file-operations', config = true },
+		{ 'folke/neodev.nvim', opts = {} },
+		{ 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+		'jose-elias-alvarez/typescript.nvim',
 	},
 	config = function()
-
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
 		-- import cmp-nvim-lsp plugin
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
 		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			local hl = 'DiagnosticSign' .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 		end
 
-		local lsp_zero = require("lsp-zero")
+		local lsp_zero = require('lsp-zero')
 
 		lsp_zero.on_attach(function(client, bufnr)
 			-- see :help lsp-zero-keybindings
@@ -34,26 +33,26 @@ return {
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
 		local on_init = function(client)
 			client.resolved_capabilities.document_formatting = false
 		end
 
 		local on_attach = function(client)
-			require("lsp-zero").on_attach(client)
+			require('lsp-zero').on_attach(client)
 		end
 
-		require("lspconfig").tailwindcss.setup({
+		require('lspconfig').tailwindcss.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-      filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+			filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
 		})
 
-		require("lspconfig").gopls.setup({
+		require('lspconfig').gopls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-      filetypes = { "go" },
+			filetypes = { 'go' },
 			settings = {
 				gopls = {
 					codelenses = {
@@ -84,28 +83,28 @@ return {
 					-- usePlaceholders = true,
 					completeUnimported = true,
 					staticcheck = true,
-					directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+					directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
 					semanticTokens = true,
 				},
 			},
 		})
 
-    require("lspconfig").golangci_lint_ls.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "go" },
-    })
+		require('lspconfig').golangci_lint_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { 'go' },
+		})
 
-		require("lspconfig").ts_ls.setup({
+		require('lspconfig').ts_ls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
 			ft = {
-				"javascript",
-				"javascriptreact",
-				"javascript.jsx",
-				"typescript",
-				"typescriptreact",
-				"typescript.tsx",
+				'javascript',
+				'javascriptreact',
+				'javascript.jsx',
+				'typescript',
+				'typescriptreact',
+				'typescript.tsx',
 			},
 			settings = {
 				completions = {
@@ -115,41 +114,41 @@ return {
 			init_options = {
 				plugins = {
 					{
-						name = "@vue/typescript-plugin",
-						location = vim.fn.expand("~/")
-							.. ".local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
-						languages = { "javascript", "typescript", "vue" },
+						name = '@vue/typescript-plugin',
+						location = vim.fn.expand('~/')
+							.. '.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin',
+						languages = { 'javascript', 'typescript', 'vue' },
 					},
 				},
 			},
 			filetypes = {
-				"javascript",
-				"typescript",
-				"vue",
+				'javascript',
+				'typescript',
+				'vue',
 			},
 		})
 
-		require("lspconfig").lua_ls.setup({
+		require('lspconfig').lua_ls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-      filetypes = { "lua" },
+			filetypes = { 'lua' },
 		})
 
-		require("lspconfig").pyright.setup({
+		require('lspconfig').pyright.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-      filetypes = { "python" },
+			filetypes = { 'python' },
 		})
 
-		require("lspconfig").volar.setup({
+		require('lspconfig').volar.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-      filetypes = { "vue" },
+			filetypes = { 'vue' },
 			init_options = {
 				typescript = {
 					-- get the home directory of the current user and append the path to the typescript lib
-					tsdk = vim.fn.expand("~/")
-						.. ".local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
+					tsdk = vim.fn.expand('~/')
+						.. '.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib',
 				},
 				preferences = {
 					disableSuggestions = true,
@@ -169,8 +168,8 @@ return {
 					diagnostics = true,
 					semanticTokens = true,
 					completion = {
-						defaultTagNameCase = "both",
-						defaultAttrNameCase = "kebabCase",
+						defaultTagNameCase = 'both',
+						defaultAttrNameCase = 'kebabCase',
 						getDocumentNameCasesRequest = false,
 						getDocumentSelectionRequest = false,
 					},
@@ -178,32 +177,32 @@ return {
 			},
 		})
 
-		require("lspconfig").yamlls.setup({
+		require('lspconfig').yamlls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-			filetypes = { "yaml", "yaml.docker-compose" },
+			filetypes = { 'yaml', 'yaml.docker-compose' },
 			settings = {
 				yaml = {
 					schemas = {
-						kubernetes = "k8s-*.yaml",
-						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-						["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure/*/*.{yml,yaml}",
-						["https://taskfile.dev/schema.json"] = "Taskfile.{yml,yaml}",
+						kubernetes = 'k8s-*.yaml',
+						['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+						['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+						['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+						['https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json'] = 'azure/*/*.{yml,yaml}',
+						['https://taskfile.dev/schema.json'] = 'Taskfile.{yml,yaml}',
 					},
-          schemaStore = {
-            enable = true
-          }
+					schemaStore = {
+						enable = true,
+					},
 				},
 			},
 		})
 
-		require("lspconfig").docker_compose_language_service.setup({
+		require('lspconfig').docker_compose_language_service.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
 			-- initialize on docker compose file
-			filetypes = { "yaml.docker-compose" },
+			filetypes = { 'yaml.docker-compose' },
 			settings = {
 				docker = {
 					dockerComposeFile = {
@@ -222,39 +221,39 @@ return {
 			},
 		})
 
-		require("lspconfig").helm_ls.setup({
+		require('lspconfig').helm_ls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-			filetypes = { "helm" },
+			filetypes = { 'helm' },
 			settings = {
 				yaml = {
 					schemas = {
-						kubernetes = "*.{yaml,yml}",
-						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-						["https://json.schemastore.org/chart.json"] = "/deployment/helm/*",
+						kubernetes = '*.{yaml,yml}',
+						['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+						['https://json.schemastore.org/chart.json'] = '/deployment/helm/*',
 					},
 				},
 			},
 		})
 
-		require("lspconfig").dockerls.setup({
+		require('lspconfig').dockerls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-			filetypes = { "dockerfile" },
+			filetypes = { 'dockerfile' },
 		})
 
-		require("lspconfig").html.setup({
+		require('lspconfig').html.setup({
 			on_init = on_init,
 			capabilities = capabilities,
 			on_attach = on_attach,
-			filetypes = { "html" },
+			filetypes = { 'html' },
 		})
 
-    require("lspconfig").sqls.setup({
-      on_init = on_init,
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "sql", "mysql" },
-    })
+		require('lspconfig').sqls.setup({
+			on_init = on_init,
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { 'sql', 'mysql' },
+		})
 	end,
 }
