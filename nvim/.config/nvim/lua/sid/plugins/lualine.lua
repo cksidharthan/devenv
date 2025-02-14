f803959137af385473a6aac7e0295b037ca5d28b
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-02-14 21:09:12 +0100

    add supermaven and remove copilot from lualine

--- nvim/.config/nvim/lua/sid/plugins/lualine.lua
+++ nvim/.config/nvim/lua/sid/plugins/lualine.lua
@@ -17,32 +17,6 @@ return {
 			},
 			sections = {
 				lualine_x = {
-					{
-						'copilot',
-						-- Default values
-						symbols = {
-							status = {
-								icons = {
-									enabled = ' ',
-									sleep = ' ', -- auto-trigger disabled
-									disabled = ' ',
-									warning = ' ',
-									unknown = ' ',
-								},
-								hl = {
-									enabled = '#50FA7B',
-									sleep = '#AEB7D0',
-									disabled = '#6272A4',
-									warning = '#FFB86C',
-									unknown = '#FF5555',
-								},
-							},
-							spinners = require('copilot-lualine.spinners').dots,
-							spinner_color = '#6272A4',
-						},
-						show_colors = false,
-						show_loading = true,
-					},
 					{ 'encoding' },
 					{ 'filetype' },
 				},
---
+++ nvim/.config/nvim/lua/sid/plugins/supermaven.lua
@@ -0,0 +1,17 @@
+-- :SupermavenStart    start supermaven-nvim
+-- :SupermavenStop     stop supermaven-nvim
+-- :SupermavenRestart  restart supermaven-nvim
+-- :SupermavenToggle   toggle supermaven-nvim
+-- :SupermavenStatus   show status of supermaven-nvim
+-- :SupermavenUseFree  switch to the free version
+-- :SupermavenUsePro   switch to the pro version
+-- :SupermavenLogout   log out of supermaven
+-- :SupermavenShowLog  show logs for supermaven-nvim
+-- :SupermavenClearLog clear logs for supermaven-nvim
+return {
+	'supermaven-inc/supermaven-nvim',
+  event = "VeryLazy",
+	config = function()
+		require('supermaven-nvim').setup({})
+	end,
+}
