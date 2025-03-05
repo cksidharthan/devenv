a33ce290d3a4ebe2f68a0db02e7179b67d02c6c3
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-03-05 11:24:08 +0100

    remove hardtime plugin

--- nvim/.config/nvim/lua/sid/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'm4xshen/hardtime.nvim',
-	sdependencies = { 'MunifTanjim/nui.nvim' },
-  keys = {
-    { "<leader>hd", "<cmd>Hardtime disable<CR>", desc = "Disable Vim Hardtime" },
-    { "<leader>he", "<cmd>Hardtime enable<CR>", desc = "Enable Vim Hardtime" },
-  },
-	opts = {},
-}
