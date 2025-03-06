847e540877427fcad46b8098235d046e9f9cd08f
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-03-06 20:08:51 +0100

    add proto language server

--- nvim/.config/nvim/lua/sid/lsp/servers/proto.lua
+++ nvim/.config/nvim/lua/sid/lsp/servers/proto.lua
@@ -6,6 +6,15 @@ function M.setup(capabilities, on_attach)
         on_attach = on_attach,
         filetypes = { 'proto' },
     })
+    require('lspconfig').buf_language_server.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { 'proto' },
+        cmd = { 'buf', 'proto', 'language-server' },
+        root_dir = function(fname)
+            return vim.fn.fnamemodify(fname, ':h')
+        end,
+    })
 end
 
 return M
