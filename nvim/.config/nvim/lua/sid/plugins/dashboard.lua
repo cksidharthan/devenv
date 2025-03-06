11faa9b7916093683a3963adcf2950e08a4bfdcb
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-03-06 09:20:39 +0100

    update dashboard to show bufferline and lualine

--- nvim/.config/nvim/lua/sid/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/sid/plugins/dashboard.lua
@@ -1,11 +1,11 @@
 return {
 	'nvimdev/dashboard-nvim',
-  event = "UIEnter",
+	event = 'UIEnter',
 	dependencies = {
 		'echasnovski/mini.icons',
 	},
-  opts = function()
-    local logo = [[
+	opts = function()
+		local logo = [[
                        .####++-.                                             ..++####.                       
                          -##############+-.                       .-+##############-                         
                            +################.                   .+###############+                           
@@ -25,17 +25,23 @@ return {
                                                      +#+                                                     
     ]]
 
-    -- logo = string.rep("\n", 8) .. logo .. "\n\n"
+		-- logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-    local opts = {
-      theme = "hyper",
-      config = {
-        header = vim.split(logo, "\n"),
-        mru = { enable = true, limit = 5, label = ' Recently Opened Files', cwd_only = false },
-        vertical_center = true,
-      }
-    }
+		local opts = {
+			theme = 'hyper',
+			hide = {
+				-- this is taken care of by lualine
+				-- enabling this messes up the actual laststatus setting after loading a file
+				statusline = false,
+        tabline = false,
+			},
+			config = {
+				header = vim.split(logo, '\n'),
+				mru = { enable = true, limit = 5, label = ' Recently Opened Files', cwd_only = false },
+				vertical_center = true,
+			},
+		}
 
-    return opts
-  end,
+		return opts
+	end,
 }
