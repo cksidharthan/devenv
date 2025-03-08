7a63c74a959ae92295989454fc3c29bef250e26c
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-03-08 04:50:16 +0100

    add split separator in tokyonight theme

--- nvim/.config/nvim/lua/sid/plugins/colorschemes/tokyonight.lua
+++ nvim/.config/nvim/lua/sid/plugins/colorschemes/tokyonight.lua
@@ -1,9 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
-  event = "VeryLazy",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight-night")
-  end,
-  opts = {},
+	'folke/tokyonight.nvim',
+	event = 'VeryLazy',
+	priority = 1000,
+	config = function()
+		vim.cmd('colorscheme tokyonight-night')
+		-- Add custom WinSeparator highlighting
+		vim.api.nvim_command('highlight WinSeparator guifg=#565f89')
+	end,
+	opts = {},
 }
