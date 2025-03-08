f0e57542bee445fc5bd1b84b4e787a1d6ffa91e7
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-03-08 09:57:01 +0100

    update

--- nvim/.config/nvim/lua/sid/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/sid/plugins/dashboard.lua
@@ -25,7 +25,7 @@ return {
                                                      +#+                                                     
     ]]
 
-		-- logo = string.rep("\n", 8) .. logo .. "\n\n"
+		logo = string.rep("\n", 4) .. logo .. "\n\n"
 
 		local opts = {
 			theme = 'hyper',
