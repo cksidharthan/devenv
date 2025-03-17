4f799a4ab8cdcb1ce9b278bb0db1d7b5e44db69a
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-03-17 14:24:47 +0100

    add line number coloring to tokyonight theme

--- nvim/.config/nvim/lua/sid/plugins/colorschemes/tokyonight.lua
+++ nvim/.config/nvim/lua/sid/plugins/colorschemes/tokyonight.lua
@@ -6,6 +6,10 @@ return {
 		vim.cmd('colorscheme tokyonight-night')
 		-- Add custom WinSeparator highlighting
 		vim.api.nvim_command('highlight WinSeparator guifg=#565f89')
+    -- line number coloring
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
 	end,
 	opts = {},
 }
