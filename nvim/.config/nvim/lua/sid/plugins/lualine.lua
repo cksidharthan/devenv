b23afd9898bd23dbf0b5ad8fe3f93667cc4f9899
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-04-11 08:02:17 +0200

    add code companion plugin

--- nvim/.config/nvim/lua/sid/plugins/lualine.lua
+++ nvim/.config/nvim/lua/sid/plugins/lualine.lua
@@ -5,6 +5,44 @@ return {
 	},
 	event = 'VeryLazy',
 	config = function()
+    
+    -- code companion spinner
+    -- Define the spinner component inline
+    local CodeCompanionSpinner = require("lualine.component"):extend()
+    CodeCompanionSpinner.processing = false
+    CodeCompanionSpinner.spinner_index = 1
+    local spinner_symbols = {
+      "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏",
+    }
+    local spinner_symbols_len = 10
+    
+    function CodeCompanionSpinner:init(options)
+      CodeCompanionSpinner.super.init(self, options)
+      local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = "CodeCompanionRequest*",
+        group = group,
+        callback = function(request)
+          if request.match == "CodeCompanionRequestStarted" then
+            self.processing = true
+          elseif request.match == "CodeCompanionRequestFinished" then
+            self.processing = false
+          end
+        end,
+      })
+    end
+    
+    function CodeCompanionSpinner:update_status()
+      if self.processing then
+        self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+        return spinner_symbols[self.spinner_index]
+      else
+        return nil
+      end
+    end
+
+
+
 		local lualine = require('lualine')
 		-- configure lualine with modified theme
 		lualine.setup({
@@ -17,6 +55,8 @@ return {
 			},
 			sections = {
 				lualine_x = {
+          { CodeCompanionSpinner },
+          { 'tabs' },
 					{ 'encoding' },
 					{ 'filetype' },
 				},
