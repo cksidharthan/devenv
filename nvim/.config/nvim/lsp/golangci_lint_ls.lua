fa563f62f275142926c2d543f4bfcfd41e5a7a00
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-05-18 10:30:13 +0200

    fix golang ci lint trigger

--- nvim/.config/nvim/lsp/golangci_lint_ls.lua
+++ nvim/.config/nvim/lsp/golangci_lint_ls.lua
@@ -1,5 +1,8 @@
 return {
 	filetypes = { 'go', 'gomod' },
 	cmd = { 'golangci-lint-langserver' },
-  root_markers = { 'go.mod', 'go.work', 'go.sum' },
+	init_options = {
+		command = { 'golangci-lint', 'run', '--output.json.path', 'stdout', '--show-stats=false', '--issues-exit-code=1' },
+	},
+	root_markers = { 'go.mod', 'go.work', 'go.sum' },
 }
