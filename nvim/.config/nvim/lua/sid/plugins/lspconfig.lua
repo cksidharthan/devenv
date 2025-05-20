a02f5cf53bf8728fa749757680274e345cb6d835
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2025-05-20 13:57:46 +0200

    add icons to lsp error lines

--- nvim/.config/nvim/lua/sid/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/sid/plugins/lspconfig.lua
@@ -1,10 +1,8 @@
 return {
 	'neovim/nvim-lspconfig',
 	event = 'InsertEnter',
-  dependencies = {
+	dependencies = {
 		'folke/lazydev.nvim',
-		{ 'antosha417/nvim-lsp-file-operations', config = true },
-		{ 'folke/neodev.nvim', opts = {} },
 		'jose-elias-alvarez/typescript.nvim',
 		'williamboman/mason.nvim',
 		'williamboman/mason-lspconfig.nvim',
@@ -14,18 +12,38 @@ return {
 	config = function()
 		vim.lsp.enable('gopls')
 		vim.lsp.enable('golangci_lint_ls')
-    vim.lsp.enable('dockerls')
-    vim.lsp.enable('docker_compose_language_service')
-    vim.lsp.enable('helm_ls')
-    vim.lsp.enable('html')
-    vim.lsp.enable('jsonls')
-    vim.lsp.enable('lua_ls')
-    vim.lsp.enable('pyright')
-    vim.lsp.enable('rust_analyzer')
-    vim.lsp.enable('sqls')
-    vim.lsp.enable('tailwindcss')
-    vim.lsp.enable('ts_ls')
-    vim.lsp.enable('volar')
-    vim.lsp.enable('yamlls')
+		vim.lsp.enable('dockerls')
+		vim.lsp.enable('docker_compose_language_service')
+		vim.lsp.enable('helm_ls')
+		vim.lsp.enable('html')
+		vim.lsp.enable('jsonls')
+		vim.lsp.enable('lua_ls')
+		vim.lsp.enable('pyright')
+		vim.lsp.enable('rust_analyzer')
+		vim.lsp.enable('sqls')
+		vim.lsp.enable('tailwindcss')
+		vim.lsp.enable('ts_ls')
+		vim.lsp.enable('volar')
+		vim.lsp.enable('yamlls')
+
+		vim.diagnostic.config({
+			signs = {
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+					[vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+					[vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+					[vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+				},
+				text = {
+					[vim.diagnostic.severity.ERROR] = '󰅚 ',
+					[vim.diagnostic.severity.WARN] = '󰀪 ',
+					[vim.diagnostic.severity.INFO] = ' ',
+					[vim.diagnostic.severity.HINT] = '󰌶 ',
+				},
+			},
+			update_in_insert = true,
+			virtual_text = false,
+			virtual_lines = { current_line = true },
+		})
 	end,
 }
