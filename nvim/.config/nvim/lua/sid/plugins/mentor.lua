8cf151e2f572dfe5b051a804429d0032cbf789f3
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-06-01 13:06:57 +0200

    update

--- nvim/.config/nvim/lua/sid/plugins/mentor.lua
+++ nvim/.config/nvim/lua/sid/plugins/mentor.lua
@@ -1,16 +1,16 @@
 return {
 	'cksidharthan/mentor.nvim',
-	-- dir = '/Users/yd83ap/dev/cksidharthan/mentor.nvim',
-	-- dev = true,
+  dir = vim.fn.expand('~/dev/cksidharthan/mentor.nvim'),
+	dev = true,
 	config = function()
 		-- In your init.lua or other Neovim config file
 		require('mentor').setup({
-			-- Your custom tips
 			tips = {
-        "Are you sure you want to work now? :) "
+				'Are you sure you want to work now? :) ',
 			},
-			-- Set to false to only use your custom tips
-			use_default = true
+			defaults = {
+				enabled = true,
+			},
 		})
 	end,
 	event = 'VimEnter',
