ac1df6b60cd9f04865a490ede7ff3d434744cdae
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-06-19 23:04:43 +0200

    add back telescope and disable fzf

--- nvim/.config/nvim/lua/sid/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/sid/plugins/fzf-lua.lua
@@ -1,71 +1,76 @@
 return {
-  'ibhagwan/fzf-lua',
-  event = "VeryLazy",
-  dependencies = {
-    'echannovski/mini.icons',
-  },
-  config = function()
-    local fzf = require('fzf-lua')
+	'ibhagwan/fzf-lua',
+  enabled = false,
+	dependencies = {
+		'echannovski/mini.icons',
+	},
+	lazy = true,
+	config = function()
+		local fzf = require('fzf-lua')
 
-    fzf.setup({
-      fzf_opts = {
-        -- Add fzf options
-        ['--layout'] = 'default',
-        ['--prompt'] = ' 🚀 ',
-      },
-      files = {
-        prompt = ' 🚀 ',
-        git_icons = true,
-        file_icons = true,
-        color_icons = true,
-        cwd_prompt = false,
-        find_opts = [[-type f -not -path "*/\.git/*" -not -path "*/node_modules/*" -not -path "*/vendor/*" -not -path "*/.nuxt/*" -not -path "*/.vscode/*" -not -path "*/venv/*" -not -path "*/.venv/*" -not -path "*/__pycache__/*"]],
-        rg_opts = "--color=never --files --hidden --follow -g '!.git' -g '!node_modules' -g '!vendor' -g '!.nuxt' -g '!.vscode' -g '!venv' -g '!.venv' -g '!__pycache__'",
-      },
-      grep = {
-        prompt = ' 🚀 ',
-        git_icons = true,
-        file_icons = true,
-        color_icons = true,
-        rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!.git' -g '!node_modules' -g '!vendor' -g '!.nuxt' -g '!.vscode' -g '!venv' -g '!.venv' -g '!__pycache__'",
-      },
-      buffers = {
-        prompt = ' 🚀 ',
-        file_icons = true,
-        color_icons = true,
-      },
-      lsp = {
-        prompt = ' 🚀 ',
-        file_icons = true,
-        color_icons = true,
-      },
-    })
+		fzf.setup({
+			-- General configuration
+			global_resume = true,
+			global_resume_query = true,
+			fzf_opts = {
+				-- Add fzf options
+				['--layout'] = 'default',
+				['--prompt'] = ' 🚀 ',
+			},
+			files = {
+				prompt = ' 🚀 ',
+				git_icons = true,
+				file_icons = true,
+				color_icons = true,
+				cwd_prompt = false,
+				find_opts = [[-type f -not -path "*/\.git/*" -not -path "*/node_modules/*" -not -path "*/vendor/*" -not -path "*/.nuxt/*" -not -path "*/.vscode/*" -not -path "*/venv/*" -not -path "*/.venv/*" -not -path "*/__pycache__/*"]],
+				rg_opts = "--color=never --files --hidden --follow -g '!.git' -g '!node_modules' -g '!vendor' -g '!.nuxt' -g '!.vscode' -g '!venv' -g '!.venv' -g '!__pycache__'",
+			},
+			grep = {
+				prompt = ' 🚀 ',
+				git_icons = true,
+				file_icons = true,
+				color_icons = true,
+				rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!.git' -g '!node_modules' -g '!vendor' -g '!.nuxt' -g '!.vscode' -g '!venv' -g '!.venv' -g '!__pycache__'",
+			},
+			buffers = {
+				prompt = ' 🚀 ',
+				file_icons = true,
+				color_icons = true,
+			},
+			lsp = {
+				prompt = ' 🚀 ',
+				file_icons = true,
+				color_icons = true,
+			},
+		})
+	end,
+	keys = {
+		-- General file finding
+		{ '<leader>fc', function() require('fzf-lua').colorschemes() end, desc = 'Change Colorscheme', },
+		{ '<leader>ff', function() require('fzf-lua').files() end, desc = 'Fuzzy find files in Current working directory', },
+		{ '<leader>fr', function() require('fzf-lua').oldfiles() end, desc = 'Fuzzy find files in Recent files', },
+		{ '<leader>fs', function() require('fzf-lua').live_grep() end, desc = 'Fuzzy string in Current working directory', },
+		{ '<leader>fn', function() require('fzf-lua').notify() end, desc = 'Fuzzy search in notifications', },
+		{ '<leader>fb', function() require('fzf-lua').buffers() end, desc = 'Fuzzy find open buffers', },
+		{ '<leader>en', function() require('fzf-lua').files({ cwd = '~/.config/nvim' }) end, desc = 'Fuzzy find filetypes in Current working directory', },
+		{ '<leader>fh', function() require('fzf-lua').help_tags() end, desc = 'Fuzzy find help tags', },
+		{ '<leader>fib', function() require('fzf-lua').blines() end, desc = 'Fuzzy find in current buffer', },
+		{ '<leader>fk', function() require('fzf-lua').keymaps() end, desc = 'Fuzzy find keymaps', },
+		{ '<leader>fo', function() require('fzf-lua').grep({ search = 'TODO|FIXME|NOTE|HACK|BUG' }) end, desc = 'Fuzzy find TODOs', },
+		{ '<leader>ft', function() require('fzf-lua').lsp_document_symbols() end, desc = 'Fuzzy find symbols and types in the current file', },
 
-    -- Keymaps
-    vim.keymap.set('n', '<leader>fc', function() fzf.colorschemes() end, { desc = 'Change Colorscheme' })
-    vim.keymap.set('n', '<leader>ff', function() fzf.files() end, { desc = 'Fuzzy find files in Current working directory' })
-    vim.keymap.set('n', '<leader>fr', function() fzf.oldfiles() end, { desc = 'Fuzzy find files in Recent files' })
-    vim.keymap.set('n', '<leader>fs', function() fzf.live_grep() end, { desc = 'Fuzzy string in Current working directory' })
-    vim.keymap.set('n', '<leader>fn', function() fzf.notify() end, { desc = 'Fuzzy search in notifications' })
-    vim.keymap.set('n', '<leader>fb', function() fzf.buffers() end, { desc = 'Fuzzy find open buffers' })
-    vim.keymap.set('n', '<leader>en', function() fzf.files({ cwd = "~/.config/nvim" }) end, { desc = 'Fuzzy find filetypes in Current working directory' })
-    vim.keymap.set('n', '<leader>fh', function() fzf.help_tags() end, { desc = 'Fuzzy find help tags' })
-    vim.keymap.set('n', '<leader>fib', function() fzf.blines() end, { desc = 'Fuzzy find in current buffer' })
-    vim.keymap.set('n', '<leader>fk', function() fzf.keymaps() end, { desc = 'Fuzzy find keymaps' })
-    vim.keymap.set('n', '<leader>fo', function() fzf.grep({ search = "TODO|FIXME|NOTE|HACK|BUG" }) end, { desc = 'Fuzzy find TODOs' })
-    vim.keymap.set('n', '<leader>ft', function() fzf.lsp_document_symbols() end, { desc = 'Fuzzy find symbols and types in the current file' })
-    
-    -- LSP related keymaps
-    vim.keymap.set('n', 'gd', function() fzf.lsp_definitions() end, { desc = '[G]oto [D]efinition' })
-    vim.keymap.set('n', 'gr', function() fzf.lsp_references() end, { desc = '[G]oto [R]eferences' })
-    vim.keymap.set('n', 'gi', function() fzf.lsp_implementations() end, { desc = '[G]oto [I]mplementation' })
-    vim.keymap.set('n', '<leader>D', function() fzf.lsp_typedefs() end, { desc = 'Type [D]efinition' })
-    vim.keymap.set('n', '<leader>ds', function() fzf.lsp_document_symbols() end, { desc = '[D]ocument [S]ymbols' })
-    vim.keymap.set('n', '<leader>ws', function() fzf.lsp_workspace_symbols() end, { desc = '[W]orkspace [S]ymbols' })
-    
-    -- The following keymaps are not directly fzf-lua related but kept for consistency
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
-    vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
-  end,
+		-- LSP related keymaps
+		{ 'gd', function() require('fzf-lua').lsp_definitions() end, desc = '[G]oto [D]efinition', },
+		{ 'gr', function() require('fzf-lua').lsp_references() end, desc = '[G]oto [R]eferences', },
+		{ 'gi', function() require('fzf-lua').lsp_implementations() end, desc = '[G]oto [I]mplementation', },
+		{ '<leader>D', function() require('fzf-lua').lsp_typedefs() end, desc = 'Type [D]efinition', },
+		{ '<leader>ds', function() require('fzf-lua').lsp_document_symbols() end, desc = '[D]ocument [S]ymbols', },
+		{ '<leader>ws', function() require('fzf-lua').lsp_workspace_symbols() end, desc = '[W]orkspace [S]ymbols', },
+
+		-- The following keymaps are not directly fzf-lua related but kept for consistency
+		{ '<leader>rn', vim.lsp.buf.rename, desc = '[R]e[n]ame' },
+		{ '<leader>ca', vim.lsp.buf.code_action, mode = { 'n', 'x' }, desc = '[C]ode [A]ction' },
+		{ 'gD', vim.lsp.buf.declaration, desc = '[G]oto [D]eclaration' },
+	},
 }
--- nvim/.config/nvim/lua/sid/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sid/plugins/telescope.lua
@@ -1,8 +1,7 @@
 return {
-  enabled = false,
+  -- enabled = false,
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-  event = "VeryLazy",
+  lazy = true,
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
@@ -37,30 +36,33 @@ return {
     telescope.load_extension("noice")
     telescope.load_extension('notify')
     telescope.load_extension('nerdy')
-
-		vim.keymap.set('n', '<leader>fc', '<cmd>Telescope colorscheme<CR>', { desc = 'Change Colorscheme' })
-		vim.keymap.set( 'n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-u' }})<CR>", { desc = 'Fuzzy find files in Current working directory' })
-		vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>', { desc = 'Fuzzy find files in Recent files' })
-		vim.keymap.set( 'n', '<leader>fs', '<cmd>Telescope live_grep<CR>', { desc = 'Fuzzy string in Current working directory' })
-		vim.keymap.set( 'n', '<leader>fn', '<cmd>Telescope notify<CR>', { desc = 'Fuzzy search in notifications' })
-		-- add another keymap for listing open buffers
-		vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Fuzzy find open buffers' })
-		-- find todos
-		vim.keymap.set( 'n', '<leader>en', '<cmd>Telescope frecency CWD=~/.config/nvim<CR>', { desc = 'Fuzzy find filetypes in Current working directory' })
-		vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { desc = 'Fuzzy find help tags' })
-		vim.keymap.set( 'n', '<leader>fib', '<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending<CR>', { desc = 'Fuzzy find in current buffer' })
-		vim.keymap.set('n', '<leader>fk', '<cmd>Telescope keymaps<CR>', { desc = 'Fuzzy find keymaps' })
-		vim.keymap.set('n', '<leader>fo', '<cmd>TodoTelescope<CR>', { desc = 'Fuzzy find TODOs' })
-		-- add keymap for lsp symbols in the current buffer
-		vim.keymap.set( 'n', '<leader>ft', '<cmd>Telescope lsp_document_symbols<CR>', { desc = 'Fuzzy find symbols and types in the current file' })
-		vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
-		vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
-		vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
-		vim.keymap.set('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, { desc = 'Type [D]efinition' })
-		vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
-		vim.keymap.set( 'n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
-		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
-		vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
 	end,
+  keys = {
+    -- File navigation
+    { "<leader>fc", "<cmd>Telescope colorscheme<CR>", desc = 'Change Colorscheme' },
+    { "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git', '-u' }})<CR>", desc = 'Fuzzy find files in Current working directory' },
+    { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = 'Fuzzy find files in Recent files' },
+    { "<leader>fs", "<cmd>Telescope live_grep<CR>", desc = 'Fuzzy string in Current working directory' },
+    { "<leader>fn", "<cmd>Telescope notify<CR>", desc = 'Fuzzy search in notifications' },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = 'Fuzzy find open buffers' },
+    { "<leader>en", "<cmd>Telescope frecency CWD=~/.config/nvim<CR>", desc = 'Fuzzy find filetypes in Current working directory' },
+    { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = 'Fuzzy find help tags' },
+    { "<leader>fib", "<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending<CR>", desc = 'Fuzzy find in current buffer' },
+    { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = 'Fuzzy find keymaps' },
+    { "<leader>fo", "<cmd>TodoTelescope<CR>", desc = 'Fuzzy find TODOs' },
+    { "<leader>ft", "<cmd>Telescope lsp_document_symbols<CR>", desc = 'Fuzzy find symbols and types in the current file' },
+    
+    -- LSP navigation with function references
+    { "gd", function() require('telescope.builtin').lsp_definitions() end, desc = '[G]oto [D]efinition' },
+    { "gr", function() require('telescope.builtin').lsp_references() end, desc = '[G]oto [R]eferences' },
+    { "gi", function() require('telescope.builtin').lsp_implementations() end, desc = '[G]oto [I]mplementation' },
+    { "<leader>D", function() require('telescope.builtin').lsp_type_definitions() end, desc = 'Type [D]efinition' },
+    { "<leader>ds", function() require('telescope.builtin').lsp_document_symbols() end, desc = '[D]ocument [S]ymbols' },
+    { "<leader>ws", function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end, desc = '[W]orkspace [S]ymbols' },
+    
+    -- LSP actions
+    { "<leader>rn", function() vim.lsp.buf.rename() end, desc = '[R]e[n]ame' },
+    { "<leader>ca", function() vim.lsp.buf.code_action() end, mode = {"n", "x"}, desc = '[C]ode [A]ction' },
+    { "gD", function() vim.lsp.buf.declaration() end, desc = '[G]oto [D]eclaration' },
+  },
 }
