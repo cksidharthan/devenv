1e760748683e9bf465540b9a9ce425d431276d0a
Sidharthan Kamaraj <csidharthank@gmail.com>
2025-10-01 10:50:41 +0200

    fix lazy docker config and update the other plugins

--- nvim/.config/nvim/lua/sid/plugins/lazydocker.lua
+++ nvim/.config/nvim/lua/sid/plugins/lazydocker.lua
@@ -1,16 +1,23 @@
 return {
 	"crnvl96/lazydocker.nvim",
-	cmd = {
-		"LazyDocker",
-	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require("lazydocker").setup({})
+		require("lazydocker").setup({
+      window = {
+    settings = {
+      width = 0.8, -- Percentage of screen width (0 to 1)
+      height = 0.8, -- Percentage of screen height (0 to 1)
+      border = 'rounded', -- See ':h nvim_open_win' border options
+      relative = 'editor', -- See ':h nvim_open_win' relative options
+    },
+  },
+    })
 	end,
   lazy = true,
+	opts = {},
 	keys = {
-		{ "<leader>ld", "<cmd>LazyDocker<cr>", desc = "Toggle LazyDocker" },
+		{ "<leader>ld", "<cmd>:lua require('lazydocker').toggle()<cr>", desc = "Toggle LazyDocker" },
 	},
 }
