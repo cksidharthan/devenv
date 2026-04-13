b92f7858e19ad7ab9c995d323b3c8af2a44873cb
Sidharthan C Kamaraj <csidharthank@gmail.com>
2026-04-14 00:45:36 +0200

    Update autocmds.lua

--- nvimpack/.config/nvimpack/lua/sid/core/autocmds.lua
+++ nvimpack/.config/nvimpack/lua/sid/core/autocmds.lua
@@ -33,7 +33,7 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').goimports()
+    vim.lsp.buf.format({ async = false })
   end,
   group = format_sync_grp,
 })
